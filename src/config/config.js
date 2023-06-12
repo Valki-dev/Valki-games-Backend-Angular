00babca21f5a7d806a844511babf54ac550bd34c
Valki-dev <valki.dev@gmail.com>
2023-06-12 19:20:12 +0200

    Enviroment conifg

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,21 @@ const config = {
     smtpHost: process.env.SMTP_HOST,
     smtpContactEmail: process.env.SMTP_CONTACTEMAIL,
     smtpPassword: process.env.SMTP_PASSWORD,
+    modeEnviroment: process.env.MODE,
+    production: {
+        database: process.env.DATABASE_PRODUCTION,
+        username: process.env.USERNAME_PRODUCTION,
+        password: process.env.PASSWORD_PRODUCTION,
+        host: process.env.HOST_PRODUCTION,
+        port: process.env.PORT_PRODUCTION,
+    },
+    develop: {
+        database: process.env.DATABASE_DEVELOP,
+        username: process.env.USERNAME_DEVELOP,
+        password: process.env.PASSWORD_DEVELOP,
+        host: process.env.HOST_DEVELOP,
+        port: process.env.PORT_DEVELOP,
+    }
 }
 
-module.exports = {config}
+module.exports = { config }
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,21 +1,36 @@
-const  Sequelize  = require('sequelize');
+const Sequelize = require('sequelize');
+const { config } = require('../config/config');
 
+let sequelize;
 
-const sequelize = new Sequelize({
-    database: 'railway',
-    username: 'root',
-    password: 'YsZdoQELeZytJsIviJoG',
-    dialect: 'mysql',
-    host: 'containers-us-west-49.railway.app',
-    port: 6157
-});
+if (config.modeEnviroment === 'production') {
+    sequelize = new Sequelize({
+        database: config.production.database,
+        username: config.production.username,
+        password: config.production.password,
+        dialect: 'mysql',
+        host: config.production.host,
+        port: Number(config.production.port)
+    });
+} else {
+    sequelize = new Sequelize({
+        database: config.develop.database,
+        username: config.develop.username,
+        password: config.develop.password,
+        dialect: 'mysql',
+        host: config.develop.host,
+        port: Number(config.develop.port)
+    });
+
+}
+
 
 
 async function connection() {
     try {
         await sequelize.sync({ alter: true });
         console.log('Connection has been established successfully');
-    } catch(error) {
+    } catch (error) {
         console.log('Unable to connect to the database: ', error);
     }
 }
